456fe9d0ae28499474cb92d21f732117a074dfce
Atul-ThakreLO <atulthakrelrat3094@gmail.com>
2026-02-20 01:39:56 +0530

    feat: complete contract

--- contracts/src/LogersWatch.sol
+++ contracts/src/LogersWatch.sol
@@ -19,11 +19,16 @@ contract LogersWatch is Ownable, AccessControl {
     error LogersWatch__AmountMustMoreThanZero();
     error LogersWatch__PermitFailed();
     error LogersWatch__ClaimFail();
+    error LogersWatch__UnAuthorizedAccount();
 
+    mapping(address creator => bool status) isVerifiedCreator;
     mapping(address creator => uint256 withdrawn) creatorWithdrawn;
-    mapping(address tokenAddress => bool status) supportedToken;
-    mapping(address user => uint256 balance) userBalance;
+    mapping(address tokenAddress => bool status) isSupportedTokens;
+    mapping(address user => uint256 balance) userDepositAmount;
     mapping(address creator => uint256 feesPaid) totalPlatformFeePaidByCreator;
+
+    address[] private supportedTokensList;
+    address[] private creatorsList;
 
     uint256 private constant PRECISION_FACTOR = 1e18;
 
@@ -31,6 +36,17 @@ contract LogersWatch is Ownable, AccessControl {
     bytes32 private MERKLE_ROOT;
     bytes32 private CREATOR_CLAIM_ROLE =
         keccak256("LOGERS_WATCH_CREATOR_ROLE_ACCESS_CHECK");
+
+    event Deposited(address indexed user, uint256 indexed amount);
+    event Claimed(address indexed creator, uint256 indexed amount);
+    event RootChange();
+    event AddCreator(address indexed creator);
+    event BanCreator(address indexed creator);
+    event AddNewTokenSupport(address indexed newToken);
+    event RemoveTokenSupport(address indexed token);
+    event GrantClaimRole(address indexed creator);
+    event RevokeClaimRole(address indexed creator);
+    event ChangePlatformFee(uint256 newFee);
 
     constructor() Ownable(msg.sender) {}
 
@@ -40,7 +56,7 @@ contract LogersWatch is Ownable, AccessControl {
     }
 
     modifier isTokenSupported(address token) {
-        if (!supportedToken[token]) {
+        if (!isSupportedTokens[token]) {
             revert LogersWatch__TokenNotSupported();
         }
         _;
@@ -69,18 +85,17 @@ contract LogersWatch is Ownable, AccessControl {
         }
 
         IERC20(token).transferFrom(msg.sender, address(this), value);
-        userBalance[msg.sender] += value;
+        userDepositAmount[msg.sender] += value;
+        emit Deposited(msg.sender, value);
     }
 
     function depositWithoutPermit(
         address token,
         uint256 value
-    ) public amountNotZero(value) {
-        if (!supportedToken[token]) {
-            revert LogersWatch__TokenNotSupported();
-        }
+    ) public amountNotZero(value) isTokenSupported(token) {
         IERC20(token).transferFrom(msg.sender, address(this), value);
-        userBalance[msg.sender] += value;
+        userDepositAmount[msg.sender] += value;
+        emit Deposited(msg.sender, value);
     }
 
     function claim(
@@ -88,6 +103,9 @@ contract LogersWatch is Ownable, AccessControl {
         uint256 totalEarnings,
         address token
     ) public onlyRole(CREATOR_CLAIM_ROLE) isTokenSupported(token) {
+        if (!isVerifiedCreator[msg.sender]) {
+            revert LogersWatch__UnAuthorizedAccount();
+        }
         bytes32 leaf = keccak256(
             bytes.concat(keccak256(abi.encode(msg.sender, totalEarnings)))
         );
@@ -105,16 +123,95 @@ contract LogersWatch is Ownable, AccessControl {
             msg.sender,
             amountToWithdraw - platformFee
         );
+        emit Claimed(msg.sender, amountToWithdraw - platformFee);
+    }
+
+    function grantClaimRoleToCreator(address creator) public onlyOwner {
+        grantRole(CREATOR_CLAIM_ROLE, creator);
+        emit GrantClaimRole(creator);
     }
 
     function revokeCreatorClaimRole(address creator) public onlyOwner {
         revokeRole(CREATOR_CLAIM_ROLE, creator);
+        emit RevokeClaimRole(creator);
+    }
+
+    function addCreator(address creator) public onlyOwner {
+        creatorsList.push(creator);
+        isVerifiedCreator[creator] = true;
+        grantClaimRoleToCreator(creator);
+        emit AddCreator(creator);
+    }
+
+    function addNewTokenSupport(address newToken) public onlyOwner {
+        supportedTokensList.push(newToken);
+        isSupportedTokens[newToken] = true;
+        emit AddNewTokenSupport(newToken);
+    }
+
+    function banCreator(address creator) public onlyOwner {
+        isVerifiedCreator[creator] = false;
+        revokeCreatorClaimRole(creator);
+        emit BanCreator(creator);
+    }
+
+    function banTokenSupport(address newToken) public onlyOwner {
+        isSupportedTokens[newToken] = false;
+        emit RemoveTokenSupport(newToken);
     }
 
     function calculateFlatformFee(
         uint256 amountToWithdraw
     ) private view returns (uint256) {
         return (amountToWithdraw * PLATFORM_FEE) / PRECISION_FACTOR;
+    }
+
+    function setMerkleRoot(bytes32 root) public onlyOwner {
+        MERKLE_ROOT = root;
+        emit RootChange();
+    }
+
+    function changePlatformFee(uint256 newFee) public onlyOwner {
+        PLATFORM_FEE = newFee;
+        emit ChangePlatformFee(newFee);
+    }
+
+    function getPlatformFee() public view returns (uint256) {
+        return PLATFORM_FEE;
+    }
+
+    function getCreators() public view returns (address[] memory) {
+        return creatorsList;
+    }
+
+    function getSupportedTokens() public view returns (address[] memory) {
+        return supportedTokensList;
+    }
+
+    function getTokenStatus(address token) public view returns (bool) {
+        return isSupportedTokens[token];
+    }
+
+    function getCreatorStstus() public view returns (bool) {
+        return isVerifiedCreator[msg.sender];
+    }
+
+    function getTotalWithdrawnByCreator(
+        address creator
+    ) public view returns (uint256) {
+        return creatorWithdrawn[creator];
+    }
+
+    function getTotalPlatformFeesPaidByCreator(
+        address creator
+    ) public view returns (uint256) {
+        return totalPlatformFeePaidByCreator[creator];
+    }
+
+    function getTotaldepositedByUser(
+        address user
+    ) public view returns (uint256) {
+        return userDepositAmount[user];
     }
 }
 
